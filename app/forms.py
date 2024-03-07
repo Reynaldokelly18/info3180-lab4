06e12f186e2413a81d83417fba520cd621b4c3b0
reynaldokelly18 <reynaldokelly8@gmail.com>
2024-03-06 23:42:34 -0500

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField
 from wtforms.validators import InputRequired
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+
+class UploadForm(FlaskForm):
+    fileField = FileField('image upload',validators=[FileRequired(),FileAllowed(['jpg', 'png', 'jpeg'], 'Illegal file detected. Ensure your file has a name and is in one of the following formats: png, jpg, jpeg.')])
